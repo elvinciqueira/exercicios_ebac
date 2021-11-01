f4620cfc30fcfa76ba1c756ed1de467521bcdd4e
elvinciqueira <elvinciqueira@gmail.com>
2021-11-01 14:59:27 -0300

    multiple of 3 and five

---
+++ javascript/multipleOf3and5.js
@@ -0,0 +1,16 @@
+/**
+ * Listando todos os números inteiros abaixo de 10 que são múltiplos de 3 ou 5, ficamos com 3, 5,
+6 e 9. A soma desses múltiplos é 23. Crie uma função que retorne a soma de todos os
+múltiplos de 5 ou 7 abaixo de 1000.
+ */
+function multipleOf3and5(number) {
+  let sum = 0;
+  for (let i = 0; i < number; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      sum += i;
+    }
+  }
+  return sum;
+}
+
+multipleOf3and5(10); // 23
