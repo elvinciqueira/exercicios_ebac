c261d813e465cb47fdda9ceb28b05a1219e848d7
elvinciqueira <elvinciqueira@gmail.com>
2021-11-01 14:50:47 -0300

    greatest common divisor

--- javascript/mdc.js
+++ javascript/mdc.js
@@ -1,8 +1,27 @@
+// Cálculo do M.D.C. pelo processo das divisões sucessivas
+
+/**
+ *
+Nesse processo efetuamos várias divisões até chegar a uma divisão exata. O divisor desta divisão é o m.d.c. Acompanhe o cálculo do m.d.c.(48,30).
+
+Regra prática:
+
+1º) dividimos o número maior pelo número menor;
+48 / 30 = 1 (com resto 18)
+
+2º) dividimos o divisor 30, que é divisor da divisão anterior, por 18, que é o resto da divisão anterior, e assim sucessivamente;
+30 / 18 = 1 (com resto 12)
+
+18 / 12 = 1 (com resto 6)
+
+12 / 6 = 2 (com resto zero - divisão exata)
+
+3º) O divisor da divisão exata é 6. Então m.d.c.(48,30) = 6.
+ */
+
 function mdc(a, b) {
   if (b === 0) return a;
   return mdc(b, a % b);
 }
 
-console.log(mdc(20, 24)); // 4
-console.log(mdc(6, 18)); // 6
-console.log(mdc(9, 12)); // 3
+console.log(mdc(48, 30)); // 6
